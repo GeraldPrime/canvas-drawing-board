0b1109e67888145f128fe6d381c09d3877d54cac
GeraldPrime <geraldokeke@gmail.com>
2023-09-08 11:46:37 +0100

    first Commit

---
+++ draw.js
@@ -0,0 +1,88 @@
+const drawBtn = document.querySelector('#draw')
+const eraseBtn = document.querySelector('#erase')
+const rangeInput = document.querySelector('input')
+const screen = document.querySelector('#screen')
+
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+//STATES
+let isActive = false
+let isDrawing = false
+let isErasing = false
+let size = +rangeInput.value
+
+
+//Functions
+const updateDrawing = (state = true) => {
+    if (state) {
+        isDrawing = state
+        isErasing = false
+        return
+    }
+
+    isDrawing = false
+    isErasing = true
+}
+
+
+
+//EVENTS
+drawBtn.addEventListener("click", () => {
+    drawBtn.classList.toggle("active")
+    eraseBtn.className = ""
+   updateDrawing()
+})
+eraseBtn.addEventListener("click", () => {
+    eraseBtn.classList.toggle("active")
+    drawBtn.className = ""
+   updateDrawing(false)
+})
+
+rangeInput.addEventListener("input", () => {
+    console.log(rangeInput)
+    size = rangeInput.value
+    screen.innerHTML= `${size}`
+})
+
+canvas.addEventListener("mousedown", () => {
+    isActive = true
+    
+})
+canvas.addEventListener("mouseup", () => {
+    isActive=false
+    
+})
+
+canvas.addEventListener("mousemove", (e) => {
+    const { x, y } = e
+    console.log({isErasing, isActive, isDrawing})
+    if (!isActive) return
+    
+    if (isDrawing) {
+        ctx.beginPath()
+        ctx.fillStyle = "#fff"
+        ctx.arc(x, y, size, 0, Math.PI * 2)
+        
+        
+        ctx.fill()
+        
+    }
+
+    if (isErasing) {
+        ctx.clearRect(x, y, size, size)
+    }
+    
+})
+
+
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+})
+
