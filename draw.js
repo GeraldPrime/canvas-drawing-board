a74a0d3e27f6fd97bf792e06159978a812b03f4d
GeraldPrime <geraldokeke@gmail.com>
2023-09-24 20:05:57 +0100

    update color 24/9

--- draw.js
+++ draw.js
@@ -1,30 +1,52 @@
-const drawBtn = document.querySelector('#draw')
-const eraseBtn = document.querySelector('#erase')
-const rangeInput = document.querySelector('input')
-const screen = document.querySelector('#screen')
-
+const drawBtn = document.querySelector("#draw")
+const eraseBtn = document.querySelector("#erase")
+const rangeInput = document.querySelector("input")
+const colorInput = document.getElementById("color")
+const screen = document.querySelector("#screen")
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 
-ctx.beginPath()
-ctx.font = " 50px Arial "
-ctx.fillStyle = "coral"
-ctx.fillText("Im Switch! ", 50, 50, 400)
 
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-//STATES
+// STATES
 let isActive = false
 let isDrawing = false
 let isErasing = false
 let size = +rangeInput.value
 
+let x
+let y
 
-//Functions
+
+
+canvas.addEventListener("pointerdown", (e) => {
+    isActive = true
+    // let x, y = e
+    x = e.offsetX
+    y = e.offsetY
+    console.log(isActive, x, y)
+})
+colorInput.addEventListener("change", (e) => {
+    if (e.target.id === 'color') {
+        color = e.target.value
+    }
+    console.log(color)
+    return color
+})
+
+
+
+
+//  FUNCTIONS
+// const updateState = (state = "") =>{
+//     isDrawing = state.toLowerCase() === "draw" ? true : false
+//     isErasing = state.toLowerCase() === "erase" ? true : false
+// }
 const updateDrawing = (state = true) => {
     if (state) {
-        isDrawing = state
+        isDrawing = true
         isErasing = false
         return
     }
@@ -32,61 +54,93 @@ const updateDrawing = (state = true) => {
     isDrawing = false
     isErasing = true
 }
+function drawCircle(x, y) {
+    ctx.beginPath()
+    ctx.arc(x, y, size, 0, Math.PI * 2)
+    ctx.fillStyle = color
+    ctx.fill()
+}
 
+function drawLine(x, y, x1, y1) {
+    ctx.beginPath()
+    ctx.moveTo(x, y)
+    ctx.lineTo(x1, y1)
+    ctx.lineWidth = size * 2
+    ctx.strokeStyle = color
+    ctx.stroke()
 
+}
 
-//EVENTS
+// EVENTS
+
 drawBtn.addEventListener("click", () => {
     drawBtn.classList.toggle("active")
     eraseBtn.className = ""
-   updateDrawing()
+    updateDrawing()
 })
+
 eraseBtn.addEventListener("click", () => {
     eraseBtn.classList.toggle("active")
     drawBtn.className = ""
-   updateDrawing(false)
+    updateDrawing(false)
 })
 
-rangeInput.addEventListener("input", () => {
-    console.log(rangeInput)
+rangeInput.addEventListener("input", () => { //this is used to increse the size of the pencil.
+    screen.innerHTML = rangeInput.value
     size = rangeInput.value
-    screen.innerHTML= `${size}%`
+    screen.innerHTML = `${size}%`
 })
 
-canvas.addEventListener("pointerdown", () => {
-    isActive = true
-    
-})
+
 canvas.addEventListener("pointerup", () => {
-    isActive=false
-    
+    isActive = false
 })
-
 canvas.addEventListener("pointermove", (e) => {
-    const { x, y } = e
-    console.log({isErasing, isActive, isDrawing})
+    const x1 = e.offsetX
+    const y1 = e.offsetY
+
     if (!isActive) return
-    
+
+
     if (isDrawing) {
-        ctx.beginPath()
-        ctx.fillStyle = "#fff"
-        ctx.arc(x, y, size, 0, Math.PI * 2)
-        
-        
-        ctx.fill()
-        
+        drawCircle(x, y)
+        drawLine(x, y, x1, y1, size * 2)
+        x = x1
+        y = y1
     }
+
 
     if (isErasing) {
+        const { x, y } = e
+
         ctx.clearRect(x, y, size, size)
     }
-    
 })
-
-
-
 window.addEventListener("resize", () => {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
 })
 
+// // Add a single event listener for both pointerdown and touchstart
+// canvas.addEventListener("pointerdown", handlePointerOrTouch);
+
+// // Event handling function
+// function handlePointerOrTouch(e) {
+//     isActive = true;
+
+//     // Check if it's a touch event (e.pointerType === "touch")
+//     if (e.pointerType === "touch") {
+//         // Handle touch event
+//         const touch = e.touches[0];
+//         x = touch.clientX - canvas.getBoundingClientRect().left;
+//         y = touch.clientY - canvas.getBoundingClientRect().top;
+//     } else {
+//         // Handle mouse event
+//         x = e.offsetX;
+//         y = e.offsetY;
+//     }
+
+//     console.log(isActive, x, y);
+// }
+
+// // Other event listeners and functions...
